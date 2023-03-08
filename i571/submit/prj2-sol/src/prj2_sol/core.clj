89d8f6258a35f4cc0bbfeec913125674a7c9a929
Mukunthan Sriram Balaji <muku0210@gmail.com>
2023-03-07 21:34:51 -0500

    clojure

--- i571/submit/prj2-sol/src/prj2_sol/core.clj
+++ i571/submit/prj2-sol/src/prj2_sol/core.clj
@@ -11,11 +11,14 @@
 ;;given a list items of OrderItem, return the total for
 ;;the order, i.e. sum n-units*unit-price over all items
 ;;must be recursive but cannot use loop or recur
- (defn items-total1 [items]
-   (cond
-     (empty? items) 0
-     :else (+ (* (:n-units (first items)) (:unit-price (first items)))
-              (items-total1 (rest items)))))
+(defn items-total1 [items]
+  (loop [remaining items
+         total 0]
+    (if (empty? remaining)
+      total
+      (recur (rest remaining)
+             (+ total (* (:n-units (first remaining)) (:unit-price (first remaining))))))))
+
 
 
 
@@ -37,6 +40,31 @@
 ;;same specs as items-total1 but must be implemented using
 ;;loop and recur
 (defn items-total2 [items]
+  (apply + (map (fn [items] (* (:n-units items) (:unit-price items))) items)))
+
+
+
+;; #4: 10-points
+;;given a list items of OrderItem return a list of all the :sku's
+;;cannot use explicit recursion
+(defn item-skus [items]
+  (reduce #(conj %1 (:sku %2)) [] items))
+
+
+;; #5: 10-points
+;;given a list items of OrderItem, return a list of skus of those elements
+;;in items having unit-price greater than price
+;;cannot use explicit recursion
+(defn expensive-item-skus [items price]
+  (->> items
+       (filter #(> (:unit-price %) price))
+       (map :sku)))
+
+
+
+;; #6: 10-points
+;;same specs as items-total1 but cannot use explicit recursion
+(defn items-total3 [items]
   (loop [total 0
          rest-items items]
     (if (empty? rest-items)
@@ -45,39 +73,12 @@
              (rest rest-items)))))
 
 
-;; #4: 10-points
-;;given a list items of OrderItem return a list of all the :sku's
-;;cannot use explicit recursion
-(defn item-skus [items]
-  (map :sku items))
-
-
-;; #5: 10-points
-;;given a list items of OrderItem, return a list of skus of those elements
-;;in items having unit-price greater than price
-;;cannot use explicit recursion
-(defn expensive-item-skus [items price]
-  (reduce #(if (> (:unit-price %2) price)
-             (conj %1 (:sku %2))
-             %1)
-          []
-          items))
-
-
-;; #6: 10-points
-;;same specs as items-total1 but cannot use explicit recursion
-(defn items-total3 [items]
-  (apply + (map #(apply * %) (map #(vector (:n-units %) (:unit-price %)) items))))
-
-
-
-
-
 
 ;; #7: 10-points
 ;;same spec as items-with-category1, but cannot use explicit recursion
 (defn items-with-category2 [items category]
-  (filter #(= (:category %) category) items))
+  (remove #(not= (:category %) category) items))
+
 
 
 ;; #8: 15-points 
@@ -91,6 +92,10 @@
   ([items]
    (reduce + (map :n-units items)))
   ([items category]
-   (reduce + (map :n-units (filter #(= category (:category %)) items)))))
+   (->> items
+        (filter #(or (nil? category) (= category (:category %))))
+        (map :n-units)
+        (reduce +))))
+
 
 
